1567ed24695d5f8390f95482c4437eafdff99bd4
Bl1nker <Alex_001@sibmail.com>
2024-09-26 11:47:09 +0700

    Task 3 done. All done

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class MyClass
+{
+    public static void Main()
+    {
+        int[] ints = { 1, 17, 3, 5, 8, 13, 23, 15 };
+        ShowArray(ints);
+        Array.Reverse(ints);
+        ShowArray(ints);
+    }
+    static void ShowArray(int[] ints)
+    {
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < ints.Length; i++)
+        {
+            Console.Write(ints[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
