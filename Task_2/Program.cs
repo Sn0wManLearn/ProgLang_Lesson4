6c3259e0d88c7e49d430c3a67bf5129ae15f8ce3
Bl1nker <Alex_001@sibmail.com>
2024-09-26 11:39:00 +0700

    Task 2 done

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class MyClass
+{
+    public static int[] RandomArray(int length)
+    {
+        Random rnd = new Random();
+        int[] arr = new int[length];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rnd.Next(100, 999);
+        }
+        return arr;
+    }
+    public static void Main()
+    {
+
+        int[] nums = RandomArray(6);
+        int count = 0;
+        Console.WriteLine("Массив:");
+        foreach (int num in nums)
+        {
+            if (num % 2 == 0) count++;
+            Console.Write(num + " ");
+        }
+        Console.WriteLine($"\nКоличество четных чисел в массиве: {count}");
+
+    }
+}
