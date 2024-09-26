20820c5d4635da421f7028b26849786c4d30b736
Bl1nker <Alex_001@sibmail.com>
2024-09-26 11:29:03 +0700

    Task 1 done

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool stop = false;
+
+        while (!stop)
+        {
+            Console.WriteLine("Введите целое число.");
+            string? input = Console.ReadLine();
+            if (input == null) continue;
+            if (input == "q") break;
+            if (Int32.TryParse(input, out int value))
+            {
+                Console.WriteLine("Введено число {0}", value);
+                int sum = 0;
+                while (value > 0)
+                {
+                    sum += value % 10;
+                    value /= 10;
+                }                
+                if (sum > 0 && sum % 2 == 0) break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
